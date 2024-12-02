8f08952dc538a9205103bd4315b6f84f88244d40
enzomazer <enzokmitani@gmail.com>
2024-12-02 16:55:23 -0400

    g
    g

--- cliente/src/pages/Registro.jsx
+++ cliente/src/pages/Registro.jsx
@@ -2,6 +2,27 @@ import { useState } from "react";
 
 export default function Registrar() {
 
+  const [nome, setnome] = useState('')
+  const [email, setemail] = useState('')
+  const registrarPessoa = async (event) => {
+    event.preventDefault()
+    try{
+      const resposta = await fetch("http://localhost:3000/usuarios", {
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify({
+        nome: nome,
+        email: email
+      })
+      })
+
+      if(resposta.ok){
+        navigation("/")
+      }
+    }catch{
+      alert("erro na aplicação")
+    }
+  }
 
   return (
         
